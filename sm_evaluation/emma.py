25741bac7042607fec90a05c8d1295e899d7d6ef
Gonzalez-Brenes, Jose P <Jose.Gonzalez-Brenes@pearson.com>
2015-01-27 23:08:45 -0500

    rewrote "emma" to use dynamic programming

--- sm_evaluation/emma.py
+++ sm_evaluation/emma.py
@@ -1,83 +1,94 @@
 __author__ = 'hy'
 
-import pandas as pd
-from scipy import stats
-import numpy as np
+novice = "novice"
+master = "master"
+incorrect = "w"
+correct = "c"
 
-from experiments.visualization import *
+class Memoize:
+    def __init__(self, f, n):
+        self.f = f
+        self.n = n
+        self.memo = {}
+    def __call__(self, *args):
+        keys = args[:self.n]
+        if not keys in self.memo:
+            self.memo[keys] = self.f(*args)
+        return self.memo[keys]
+
+# Needs a call to forward_synthetic_aux.memo = {}
+def forward_synthetic_aux(t, k, o, initial, emission, transition):
+    if t == 0:
+        return initial[k] * emission[k][o]
+    elif t < 1:
+        raise RuntimeError ("Invalid argument t ({:0.2f}) should be >= 0".format(t))
+    else:
+        outputs= emission.items()[0][1].keys()
+        states = emission.keys()
+        ans = 0
+        for k1 in states:
+            for o1 in outputs:
+                ans += forward_synthetic_aux(t-1, k1, o1, initial, emission, transition) * transition[k1][k]
+        return ans * emission[k][o]
+
+forward_synthetic_aux = Memoize(forward_synthetic_aux, 3)
+
+def forward_synthetic(t, k0, learning_rate, guess, slip, forget=0):
+    # Setup helpful variables / memoization:
+    transition, emission, initial = prepare_arrays(k0, learning_rate, guess, slip, forget) # convenience method
+    outputs= emission.items()[0][1].keys()
+    states = emission.keys()
+
+    print "init=", initial
+    print "emit=", emission
+    print "trans=", transition
+    print "alphabet=", outputs
+
+    forward_synthetic_aux.memo = {}
+
+    # Calculate forward probs, while exhaustively generating synthetic data
+    a = []
+    for k1 in states:
+        for o1 in outputs:
+            result = forward_synthetic_aux(t,k1, o1, initial, emission, transition)
+            a.append(result)
+            print "*", k1, o1, result
+    print sum(a)
+    pc = [0] * (t+1)
+    pk = [0] * (t+1)
+    for ((t1, s1, e1), p) in forward_synthetic_aux.memo.iteritems():
+        if s1 == master:
+            pk[t1] += p
+        if e1 == correct:
+            pc[t1] += p
+
+    print pc
+    print pk
 
 
-# TODO:
-# when pG+pS<1, pcorrect should be non-decreasing, double check!
 
-# Compute Score:
-# For one kc:
-# 1. Given the true parameters (logged when generating synthetic data), all the hypothetical sequences should be generated (Emma doesn't generate and pick one sequence to extend but consider all possible extensions). If we limit sequence length <= 10, then there are at most 2^10 possible sequences. For each sequence, every point's actual correctness, p(know) and p(correct) should be recorded.
-# 2. For each sequence, compute score by checking p(know)>=0.6 (etc.), and then weight each sequence's score by its likelihood.
-# 3. Compute mean over each sequence score and get the theoretical score of this KC.
+def prepare_arrays(k0, learning_rate, guess, slip, forget):
+    transmat     = {master:{master:-1, novice:-1}, novice:{master:-1, novice:-1}}
+    emissionprob = {master:{incorrect:-1, correct:-1}, novice:{incorrect:-1, correct:-1}}
+    startprob    = {master:-1, novice:-1}
 
-verbose = True
-maximum_length = 100
-output_posterior_pL = False
+    transmat[master][novice] = forget
+    transmat[master][master]  = 1-forget
+    transmat[novice][master]     = learning_rate
+    transmat[novice][novice]    = 1- learning_rate
+    emissionprob[novice][correct]    = guess
+    emissionprob[novice][incorrect]  = 1-guess
+    emissionprob[master][incorrect]  = slip
+    emissionprob[master][correct]    = 1-slip
+    startprob[master] = k0
+    startprob[novice] = 1 - k0
 
-
-
-
-def expOppNeed(pL, pT, pG, pS, threshold, p_path=1, eita=0.0001, length=0, use_pL_to_judge_mastery=False):#0.0000001):
-    length += 1
-    p_c_given_pL = pG * (1 - pL) + (1 - pS) * pL #not p(C|K)!
-
-    if verbose:
-        print "{}\tp_path: {:.2f},  pL:{:.2f}, pC:{:.2f}".format( length, p_path, pL, p_c_given_pL)
-
-    # Stopping conditions:
-    if (p_path < eita) or (maximum_length > 0 and length >= maximum_length):  # simulation has gone too long
-        if verbose:
-            print length, "\tend 0"
-        return 0
-    elif (use_pL_to_judge_mastery and pL >= threshold) or ( not use_pL_to_judge_mastery and p_c_given_pL >= threshold): # mastery reached
-        if verbose:
-            print length, "\tend 1"
-        return 1
-
-    # Continue:
-    #p_c_pLnext = (1 - pS) * (pL + pT * (1 - pL))
-    p_c_pLnext = (1-pL)*pT*pG + (1-pS)*pL
-    p_c_pnLnext = pG * (1 - pT) * (1 - pL)
-    p_Lnext_given_c = p_c_pLnext / (p_c_pLnext + p_c_pnLnext)
-    p_path_c = p_path * p_c_given_pL
-
-    EO_c = expOppNeed(p_Lnext_given_c, pT, pG, pS, threshold, p_path=p_path_c, length=length)
-
-    #JPG
-    p_w_given_pL = 1 - p_c_given_pL
-    #p_w_given_pL = (1 - pG) * (1 - pL) + pS * pL
-    #assert( p_w_given_pL ==  1 - p_c_given_pL)
-    #print p_w_given_pL + p_c_given_pL
-
-    #p_w_pLnext = pS * (pL + pT * (1 - pL))
-    p_w_pLnext = (1-pL)*pT*(1-pG) + pS*pL
-    #p_w_pnLnext = (1 - pG) * (1 - pT) * (1 - pL)
-    p_w_pnLnext = (1-pL)*(1-pT)*(1-pG) + 0
-    p_Lnext_given_w = p_w_pLnext /  (p_w_pLnext + p_w_pnLnext)
-    p_path_w = p_path * p_w_given_pL
-    EO_w = expOppNeed(p_Lnext_given_w, pT, pG, pS, threshold, p_path=p_path_w, length=length)
-
-    effort = (1 + p_c_given_pL *EO_c + p_w_given_pL * EO_w)#(1 + p_c_given_pL*EO_c*flag + p_w_given_pL * EO_w* flag)
-    if verbose:
-        print "*  {}  {:.3f}  {:.3f}".format(length, pL, p_c_given_pL)
-    return effort
-
+    return transmat, emissionprob, startprob
 
 
 
 def main():
-    print expOppNeed(pL=0.3, pT=0.25, pG=0.3, pS=0.3, threshold=0.6, use_pL_to_judge_mastery=False)
-    #compare(path+"syn_white_kt_on_train.csv", path+"syn_emma_effort_by_pcorrect.csv", "effort")
-    # compute_effort = False
-    # compute_emma()
-    # compute_score()
-    #compare(path+"syn_white_kt_on_train.csv", path+"syn_emma_score_by_pcorrect.csv", "score")
+    forward_synthetic(t=5, k0=0.3, learning_rate=0.25, guess=0.3, slip=0.3)
 
 
 if __name__ == "__main__":
